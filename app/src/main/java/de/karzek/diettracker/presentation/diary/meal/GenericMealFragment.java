dd2df70f6bf4895c8c2074bc27882a48ab93241f
MarjanaKarzek <marjanakarzek@users.noreply.github.com>
2018-05-28 21:19:44 +0200

    [#15] feat (*) -> add basic ui for generic meal fragment

--- app/src/main/java/de/karzek/diettracker/presentation/diary/meal/GenericMealFragment.java
+++ app/src/main/java/de/karzek/diettracker/presentation/diary/meal/GenericMealFragment.java
@@ -1,5 +1,23 @@
 package de.karzek.diettracker.presentation.diary.meal;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import javax.inject.Inject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import de.karzek.diettracker.R;
+import de.karzek.diettracker.presentation.TrackerApplication;
+import de.karzek.diettracker.presentation.common.BaseFragment;
+import de.karzek.diettracker.presentation.diary.DiaryContract;
+
 /**
  * Created by MarjanaKarzek on 28.05.2018.
  *
@@ -7,5 +25,43 @@ package de.karzek.diettracker.presentation.diary.meal;
  * @version 1.0
  * @date 28.05.2018
  */
-public class GenericMealFragment {
+public class GenericMealFragment extends BaseFragment {
+
+    @Override
+    protected void setupFragmentComponent() {
+        //TrackerApplication.get(getContext()).createGenericMealComponent().inject(this);
+    }
+
+    //@Inject
+    //GenericMealContract.Presenter presenter;
+
+    @BindView(R.id.recycler_view) RecyclerView recyclerView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_generic_meal, container, false);
+        ButterKnife.bind(this,view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //presenter.setView(this);
+        //presenter.start();
+    }
+
+    /*@Override
+    public void setPresenter(DiaryContract.Presenter presenter) {
+        this.presenter = presenter;
+    }*/
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        //presenter.finish();
+        TrackerApplication.get(getContext()).releaseDiaryComponent();
+    }
 }
